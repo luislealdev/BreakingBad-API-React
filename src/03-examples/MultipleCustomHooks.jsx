cc5214bab0a64a2e46dc2cfc8d5e080cb0e205b8
luislealdev <luisrrleal@gmail.com>
2022-06-27 19:59:49 -0500

    first deploy

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,25 @@
+import { Loading, Quote } from "../components";
+import { useFetch,useCounter } from "../hooks"
+
+export const MultipleCustomHooks = () => {
+
+    const {counter, increment} = useCounter();
+    
+    const {data, isLoading, hasError} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const {quote, author} = !!data && data[0];
+
+
+
+  return (
+    <>
+        <h1>BreakingBad Quotes</h1>
+        <hr/>
+        {isLoading?<Loading/>:<Quote quote={quote} author={author}/>}
+
+        <button 
+        className="btn btn-primary"
+        onClick={() =>increment()}>
+        Next quote</button>
+    </>
+  )
+}
---
+++ src/components/Quote.jsx
@@ -0,0 +1,15 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const Quote = ({ quote, author }) => {
+
+
+  return (
+    <>
+      <blockquote className="blockquote text-end">
+        <p className="mb-1">{quote}</p>
+        <footer className="blockquote-footer">{author}</footer>
+      </blockquote>
+
+    </>
+  );
+};
